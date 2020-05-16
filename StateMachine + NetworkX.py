fa727e256247c0d75a43c76e9475151207247319
MaikGon <michal.gontarczyk@wp.pl>
2020-05-16 17:07:38 +0200

    Some changes

--- StateMachine + NetworkX.py
+++ StateMachine + NetworkX.py
@@ -104,11 +104,10 @@ def main():
             # add transition to source state
             master_states[from_idx].transitions.append(transition)
 
-    path_1 = ["t_0_1", "t_1_2", "t_2_1", "t_1_3", "t_3_1"]
-    path_2 = ["t_0_1", "t_1_3", "t_3_1", "t_1_6"]
-    path_3 = ["t_0_1", "t_1_3", "t_3_1", "t_1_4", "t_4_1", "t_1_5", "t_5_1"]
+    path_1 = ["t_0_1", "t_1_3", "t_3_1", "t_1_4", "t_4_1", "t_1_2", "t_2_1", "t_1_5", "t_5_1", "t_1_3", "t_3_1", "t_1_6"]
 
-    paths = [path_1, path_2, path_3]
+
+    paths = [path_1]
 
     for path in paths:
 
@@ -225,7 +224,7 @@ def main():
                 prev = "b4"
 
             if supervisor.current_state.value == "b5":
-                print("Robot układa produkt na 3 miejscu, pudełko pełne")
+                print("Robot układa produkt na 3 miejscu, pudełko pełne, podstawienie nowego pudełka")
                 nx.draw_networkx_nodes(G, pos, nodelist=range(6, 7), node_size=400)
                 nx.draw_networkx_nodes(G, pos, nodelist=range(0, 5), node_size=400)
                 nx.draw_networkx_nodes(G, pos, nodelist=range(5, 6), node_size=400, node_color='r')
@@ -262,26 +261,43 @@ def main():
 def run():
     model = Puma560()
     # define joint positions
-    to_pick = [0.0, 90.0, 0.0, 0.0, 0.0, 0.0]               #podejscie do podniesienia
     pick = [0.0, 180.0, 0.0, 0.0, 0.0, 0.0]                 #podniesienie
-    wait = [90.0, 90.0, 0.0, 0.0, 0.0, 0.0]                 #pozycja wyjsciowa
+    wait = [0.0, 90.0, 0.0, 0.0, 0.0, 0.0]                  #pozycja wyjsciowa
     to_leave_good = [180.0, 90.0, 0.0, 0.0, 0.0, 0.0]       #podejscie do zaladowania
     leave_good = [180.0, 180.0, 0.0, 0.0, 0.0, 0.0]         #zaladowanie
-    to_leave_bad = [270.0, 90.0, 0.0, 0.0, 0.0, 0.0]        #podejscie do odrzucenia
-    leave_bad = [270.0, 180.0, 0.0, 0.0, 0.0, 0.0]          #odrzucenie
+    to_leave_bad = [-90.0, 90.0, 0.0, 0.0, 0.0, 0.0]        #podejscie do odrzucenia
+    leave_bad = [-90.0, 180.0, 0.0, 0.0, 0.0, 0.0]          #odrzucenie
 
-    path1 = move_j(model, wait, to_pick)
-    path2 = move_j(model, to_pick, pick)
-    path3 = move_j(model, pick, to_pick)
-    path4 = move_j(model, to_pick, to_leave_good)
-    path5 = move_j(model, to_leave_good, leave_good)
-    path6 = move_j(model, leave_good, to_leave_good)
-    path7 = move_j(model, to_leave_good, wait)
-    path8 = move_j(model, to_pick, to_leave_bad)
-    path9 = move_j(model, to_leave_bad, leave_bad)
-    path10 = move_j(model, leave_bad, wait)
+    path1 = move_j(model, wait, pick, path_length=80)
+    path2 = move_j(model, pick, wait, path_length=50)
+    path3 = move_j(model, wait, to_leave_good, path_length=40)
+    path4 = move_j(model, to_leave_good, leave_good, path_length=80)
+    path5 = move_j(model, leave_good, to_leave_good, path_length=50)
+    path6 = move_j(model, to_leave_good, wait, path_length=40)
+    path7 = move_j(model, wait, to_leave_bad, path_length=40)
+    path8 = move_j(model, to_leave_bad, leave_bad, path_length=80)
+    path9 = move_j(model, leave_bad, to_leave_bad, path_length=50)
+    path10 = move_j(model, to_leave_bad, wait, path_length=40)
 
-    path = np.concatenate((path1, path2, path3, path4, path5, path6, path7, path1, path2, path3, path8, path9, path10), axis=0)
+    print("\n Symulacja robopy")
+    print("1. Podniesienie przedmiotu")
+    print("2. Odstawienie 1 przedmiotu na paletę")
+    print("3. Powrót do pozycji home")
+    print("4. Podniesienie przedmiotu")
+    print("5. Odstawienie 2 przedmiotu na paletę")
+    print("6. Powrót do pozycji home")
+    print("7. Podniesienie przedmiotu")
+    print("8. Odrzucenie wadliwego produktu")
+    print("9. Powrót do pozycji home")
+    print("10. Podniesienie przedmiotu")
+    print("11. Odstawienie 3 przedmiotu na paletę")
+    print("12. Powrót do pozycji home")
+    print("13. Podniesienie przedmiotu")
+    print("14. Odstawienie 1 przedmiotu na paletę")
+    print("15. Powrót do pozycji home")
+    print("16. Awaria")
+
+    path = np.concatenate((path1, path2, path3, path4, path5, path6, path1, path2, path3, path4, path5, path6, path1, path2, path7, path8, path9, path10, path1, path2, path3, path4, path5, path6, path1, path2, path3, path4, path5, path6), axis=0)
 
     model.animate(stances=path, frame_rate=30, unit='deg')
 
